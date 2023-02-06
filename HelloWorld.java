c39533c77ee3e72e5f8bb522bf4946c20b2cd567
nictheriscer <98423854+nictheriscer@users.noreply.github.com>
2023-02-06 17:40:36 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,4 @@
-/**.
+/**
 
 
 * My program says Hello World
@@ -15,14 +15,12 @@
 
 * @since   2023-02-06
 
-
 */
-package package-info.java;
 
-
+package info.java;
 
 class HelloWorld {
-  private static void main(String[] args) {
+  public static void main(String[] args) {
     // Print Hello World
     System.out.println("Hello Alex De Meo");
     System.out.println("You are looking mighty fine today");
