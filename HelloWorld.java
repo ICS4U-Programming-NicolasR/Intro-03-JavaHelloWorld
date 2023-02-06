6f36af2c4a86c474852dc4ce85bddb2d3500b17f
nictheriscer <98423854+nictheriscer@users.noreply.github.com>
2023-02-06 17:31:15 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -22,7 +22,7 @@ package java.awt;
 
 
 class HelloWorld {
-  static void main(String[] args) {
+  private static void main(String[] args) {
     // Print Hello World
     System.out.println("Hello Alex De Meo");
     System.out.println("You are looking mighty fine today");
