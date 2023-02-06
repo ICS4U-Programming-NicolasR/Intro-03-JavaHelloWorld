c9e5a33a0962e36feea42a2dd34f6b2be0c0e288
Nicolas Riscalas <nicolas.riscalas@stu.ocsb.ca>
2023-02-06 21:58:48 +0000

    hfjdksahfjk

---
+++ HelloWorld.java
@@ -0,0 +1,30 @@
+/**
+
+
+* My program says Hello World
+
+
+*
+
+
+* @author  Nicolas Riscalas
+
+
+* @version 1.0
+
+
+* @since   2023-02-06
+
+
+*/
+
+
+
+
+class HelloWorld {
+  public static void main(String[] args) {
+    // Print Hello World
+    System.out.println("Hello Alex De Meo");
+    System.out.println("You are looking mighty fine today");
+  }
+}
